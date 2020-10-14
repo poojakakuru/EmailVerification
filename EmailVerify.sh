18d6883e7484a51d5ef6ee5ac74e82107f3c8319
poojakakuru <poojareddykakuru@gmail.com>
2020-10-14 10:21:33 +0530

    verifying TLD is added

--- EmailVerify.sh
+++ EmailVerify.sh
@@ -4,11 +4,11 @@ echo "Welcome to email verification program"
 
 read -p "Enter the Email-id:" email
 
-#verifying .xyz
+#verifying TLD
 
-emailPat="^[A-Za-z0-9]+([._+-][0-9a-zA-z]+)?@[a-zA-Z]+.[a-zA-Z]{2,4}"
+emailPattern="^[A-Za-z0-9]+([._+-][0-9a-zA-z]+)?@[a-zA-Z]+.[a-zA-Z]{2,4}([.][a-zA-Z]{2})?"
 
-if [[ $email =~ $emailPat ]]
+if [[ $email =~ $emailPattern ]]
 then
    echo "valid"
 else
