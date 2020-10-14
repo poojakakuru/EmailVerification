667cfae2e7a6f1799dbd11e63d40db06c5637748
poojakakuru <poojareddykakuru@gmail.com>
2020-10-14 09:58:08 +0530

    Verifying the starting is done

--- EmailVerify.sh
+++ EmailVerify.sh
@@ -1,3 +1,16 @@
 #!/bin/bash -x
 
 echo "Welcome to email verification program"
+
+read -p "Enter the Email-id:" email
+
+#verifying starting part
+
+emailPat="^[A-Za-z0-9]+"
+
+if [[ $email =~ $emailPat ]]
+then
+   echo "valid"
+else
+   echo "Invalid"
+fi
